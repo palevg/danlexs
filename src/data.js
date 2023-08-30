59ce3815ae639e3aa4aa9b8dfba941d4fe6d37ea
palevg <palevg@gmail.com>
2023-08-30 12:41:21 +0200

    version2

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const texts = [
+    {
+        miHome: "Home", miAbout: "O nas", miTech: "Technologie", miOffer: "Nasza oferta", miContact: "Kontakt",
+        mProj: "PROJEKTOWANIE I PRODUKCJA Z LAMINATU POLIESTROWO-SZKLANEGO",
+        aboutDesc: "25 lat doświadczenia w projektowaniu i produkcji kompozytowych podzespołów i system do wnętrz i zewnętrznych części lokomotyw wagonów i tramwajów. Realizowane projekty w Niemczech, Rumunii, Bułgarii i na Ukrainie. Własne biuro projektowe i nowoczesna produkcja w dwóch krajach. Produkujemy 30 ton wyrobów z włókna szklanego miesięcznie.",
+        teHead: "Technologia FRP polimery wzmocnione włóknem", te1h: "Laminat ręczny HLU",
+        te1desc: "Polegają na układaniu zbrojenia w formie i przesycaniu żywicą z dodatkiem utwardzacza za pomocą wałków, pędzli itp. Po przesyceniu i odpowietrzeniu zbrojenia żywica żeluje, a następnie utwardza się.",
+        te2h: "Infuzję VIP (Vacuum Infusion Process)", te3h: "Natrysk laminatow",
+        te2desc: "Proces infuzji polega na wytwarzaniu podciśnienia pomiędzy ‘workiem próżniowym’, a formą. Dzięki temu jest możliwe uzyskanie efektu, kiedy to żywica jest niemal zasysana, w efekcie czego powstaje szkielet, który można poddawać dalszej obróbce.",
+        te3desc: "Technologia polegająca na nakładaniu żywicy oraz włókna szklanego pod ciśnieniem za pomocą odpowiedniego urządzenia (rodzaj pistoletu malarskiego) na formę pokrytą wcześniej żelkotem.",
+        of1: "Projektowanie i produkcja kabin lokomotyw", of2: "Produkcja elementów wyposażenia wnętrz i obudowy kamperów",
+        of3: "Projektowanie i produkcja wnętrz wagonów i tramwajów", of4: "Produkcja silosów rolniczych",
+        of5: "Produkcja modulej sanitarnych i higienicznych", of6: "Produkcja elementów placów zabaw",
+        prHead: "Dlaczego składane są u nas zamówienia", prReas1: "mamy zrównoważone ceny, zawsze niższe niż konkurenci zatrudniamy specjalistów, którzy przeszli certyfikację ISO 9001:2015",
+        prReas2: "wyprodukujemy produkty według Twojego szkicu, rysunku lub próbki", prReas3: "gwarantujemy jakość produktów, terminowość dostaw i zgodność ze wszystkimi normami",
+        opf: "Spółka z ograniczoną odpowiedzialnością", str: "ul.", city: "Warszawa", country: "Polska"
+    },
+    {
+        miHome: "Home", miAbout: "Über uns", miTech: "Technologien", miOffer: "Unser Angebot", miContact: "Kontakt",
+        mProj: "ENTWURF UND HERSTELLUNG VON POLYESTER-GLAS-LAMINAT",
+        aboutDesc: "25 Jahre Erfahrung in der Entwicklung und Produktion von Verbundkomponenten und -systemen für den Innen- und Außenbereich von Lokomotiven, Waggons und Straßenbahnen. Realisierte Projekte in Deutschland, Rumänien, Bulgarien und der Ukraine. Eigenes Designbüro und moderne Produktion in zwei Ländern. Wir produzieren 30 Tonnen Glasfaserprodukte pro Monat.",
+        teHead: "Faserverstärkte Polymere mit FRP-Technologie", te1h: "Handlaminat HLU",
+        te1desc: "Sie bestehen darin, die Bewehrung in eine Form zu legen und sie mithilfe von Rollen, Bürsten usw. mit Harz unter Zugabe eines Härters zu tränken. Nachdem die Bewehrung gesättigt und entlüftet ist, geliert das Harz und härtet dann aus.",
+        te2h: "VIP-Infusion (Vakuum-infusionsverfahren)", te3h: "Laminatspritzen",
+        te2desc: "Beim Infusionsprozess wird zwischen dem „Vakuumbeutel“ und der Form ein Vakuum erzeugt. Dadurch ist es möglich, einen Effekt zu erzielen, bei dem das Harz fast aufgesaugt ist, wodurch ein Gerüst entsteht, das weiterverarbeitet werden kann.",
+        te3desc: "Technologie, bei der Harz und Glasfasern unter Druck mit einem geeigneten Gerät (einer Art Lackierpistole) auf eine zuvor mit Gelcoat bedeckte Form aufgetragen werden.",
+        of1: "Design und Produktion von Lokomotivkabinen", of2: "Herstellung von Inneneinrichtungen und Gehäusen für Wohnmobile",
+        of3: "Design und Produktion von Waggon- und Straßenbahninterieurs", of4: "Herstellung von landwirtschaftlichen Silos",
+        of5: "Produktion von Sanitär- und Hygienemodulen", of6: "Herstellung von Spielplatzelementen",
+        prHead: "Warum bei uns Bestellungen aufgegeben werden", prReas1: "Wir haben ausgewogene Preise, die immer niedriger sind als die der Konkurrenz. Wir beschäftigen Spezialisten, die die ISO 9001:2015-Zertifizierung bestanden haben",
+        prReas2: "Wir fertigen Produkte nach Ihrer Skizze, Zeichnung oder Ihrem Muster", prReas3: "Wir garantieren Produktqualität, pünktliche Lieferungen und die Einhaltung aller Standards",
+        opf: "Gesellschaft mit beschränkter Haftung", str: "st.", city: "Warschau", country: "Polen"
+    },
+    {
+        miHome: "Home", miAbout: "About us", miTech: "Technologies", miOffer: "Our offer", miContact: "Contact",
+        mProj: "DESIGN AND PRODUCTION OF POLYESTER-GLASS LAMINATE",
+        aboutDesc: "25 years of experience in the design and production of composite components and systems for the interior and exterior of locomotives, wagons and trams. Implemented projects in Germany, Romania, Bulgaria and Ukraine. Own design office and modern production in two countries. We produce 30 tons of fiberglass products per month.",
+        teHead: "FRP technology fiber reinforced polymers", te1h: "Hand laminate HLU",
+        te1desc: "They consist in placing the reinforcement in a form and saturating it with resin with the addition of a hardener using rollers, brushes, etc. After the reinforcement is saturated and vented, the resin gels and then hardens.",
+        te2h: "VIP Infusion (Vacuum Infusion Process)", te3h: "Laminate spraying",
+        te2desc: "The infusion process involves creating a vacuum between the 'vacuum bag' and the mould. Thanks to this, it is possible to obtain an effect when the resin is almost sucked, resulting in a skeleton that can be further processed.",
+        te3desc: "Technology consisting in applying resin and glass fiber under pressure with the use of an appropriate device (a kind of paint gun) on a form previously covered with gelcoat.",
+        of1: "Design and production of locomotive cabins", of2: "Production of interior fittings and housing for motorhomes",
+        of3: "Design and production of wagon and tram interiors", of4: "Production of agricultural silos",
+        of5: "Production of sanitary and hygienic modules", of6: "Production of playground elements",
+        prHead: "Why orders are placed with us", prReas1: "we have balanced prices, always lower than competitors we employ specialists who have passed ISO 9001:2015 certification",
+        prReas2: "we will produce products according to your sketch, drawing or sample", prReas3: "we guarantee product quality, timely deliveries and compliance with all standards",
+        opf: "Limited liability company", str: "st.", city: "Warsaw", country: "Poland"
+    }
+];
